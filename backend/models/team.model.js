7c58e12d0c8e72470936f27da55c3b522d96e9be
mikanogami <mikanogami@gmail.com>
2021-01-24 16:51:30 -0800

    Created team Schema and router for team

--- backend/models/team.model.js
+++ backend/models/team.model.js
@@ -1,1 +1,17 @@
-// TO FILL IN
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const teamSchema = new Schema({
+    teamName: {type: String, required: true, unique: true},
+    users: {type: Array, required: false},
+    subTeams: {type: Array, required: false}, 
+    applicants: {type: Array, required: false},
+    calendars: {type: Array, required: false}
+},
+{
+    timestamps: true
+})
+
+const Team = mongoose.model("Team", teamSchema);
+
+module.exports = Team;
--- backend/routes/teams.js
+++ backend/routes/teams.js
@@ -1,8 +1,27 @@
 const router = require("express").Router();
 let Team = require("../models/team.model");
 
+const bodyParser = require("body-parser")
+router.use(bodyParser.json());
+
 router.route("/").get((req, res) => {
   res.send("You're at the Teams endpoint");
 });
+
+router.route("/").post((req, res) => {
+
+  const newTeam = new Team({
+    teamName: req.body.teamName,
+    users: req.body.users,
+    subTeams: req.body.subTeams,
+    applicants: req.body.applicants,
+    calendars: req.body.calendars,
+    date: Date.parse(req.body.date),
+  });
+
+  newTeam.save()
+  .then(() => res.json(newTeam.__id))
+  .catch(err => res.status(400).json('Error: ' + err));
+})
 
 module.exports = router;
