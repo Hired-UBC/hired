76c1d82bd4f00ee3749544d3f9397f565b9ba1c8
hanyu0104 <ubchanyu@gmail.com>
2021-02-04 10:27:10 +0900

    Calendar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Form from "react-bootstrap/Tab";
 import Button from "react-bootstrap/Tab";
+import CalendarButton from "./components/Calendar/CalendarButton.js";
+import CalendarGrid from "./components/Calendar/Calendar_Old";
+import CalendarData from "./components/Calendar/CalendarData";
+import Calendar from "react-calendar";
+import Popover from "./components/Calendar/Popover";
+import "react-calendar/dist/Calendar.css";
 
 function App() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [finalDate, setFinalDate] = useState(new Date());
+  startDate.setHours(0, 0, 0, 0);
+  finalDate.setHours(0, 0, 0, 0);
+  const makeStartDate = (startDate) => setStartDate(startDate);
+  const makeFinalDate = (finalDate) => setFinalDate(finalDate);
 
   return (
-    <Register />
-
+    <>
+      {/* <Popover clicked={true} firstName="Han" lastName="Yu" /> */}
+      {/* <CalendarButton
+        active={true}
+        clicked={false}
+        time="10:00"
+        firstName="Han"
+        lastName="Yu"
+      /> */}
+      <div>
+        <Calendar onChange={makeStartDate} value={startDate} />
+      </div>
+      <div>
+        <Calendar onChange={makeFinalDate} value={finalDate} />
+      </div>
+      {startDate.toString()}
+      {finalDate.toString()}
+      {finalDate - startDate}
+      <CalendarData
+        startDate={startDate}
+        finalDate={finalDate}
+        dateDiff={finalDate - startDate}
+        startHour={10}
+        startMin={0}
+        finalHour={16}
+        finalMin={30}
+        duration={30}
+      />
+    </>
   );
 }
 
---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,410 @@
+import React, { useState, setState } from "react";
+import CalendarButton from "./CalendarButton.js";
+import Calendar from "react-calendar";
+import * as GrIcons from "react-icons/gr";
+import { ContactMailOutlined } from "@material-ui/icons";
+import styled from "styled-components";
+
+//Styled Components
+const GridContainer = styled.div`
+  margin-top: 5%;
+  margin-left: 5%;
+  max-width: 70%;
+  max-height: 700px;
+  gap: 0;
+  display: grid;
+  grid-template-columns: repeat(${(props) => props.columns}, calc(100% / 7));
+  grid-template-rows: 20px, 15px;
+  grid-auto-rows: 1fr;
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const HeaderWrapper = styled.div`
+  height: 4em;
+  display: flex;
+  align-items: center;
+`;
+
+const StyledDates = styled.div`
+  font-size: 2.5em;
+  font: open-sans;
+  font-weight: 500;
+  color: #4f4f4f;
+  text-align: center;
+  vertical-align: center;
+  align-items: center;
+  justify-contents: center;
+  border-left: solid 1px #e0e0e0;
+  border-right: solid 1px #e0e0e0;
+  border-bottom: solid 1px #e0e0e0;
+`;
+
+const StyledWeek = styled.h1`
+  font: open-sans;
+  backgound-color: blue;
+  font-size: 2.5em;
+  display: flex;
+  text-align: center;
+  vertical-align: center;
+  margin-top 5%;
+  margin-left 5%;
+`;
+
+const StyledYear = styled.span`
+  font-weight: 0.8em;
+  text-align: center;
+  vertical-align: center;
+  font-size: 1em;
+  font: sans-serif;
+`;
+
+const StyledDays = styled.div`
+  color: #4f4f4f;
+  font-size: 1.5em;
+  font: sans-serif;
+  text-align: center;
+  vertical-align: center;
+  align-items: center;
+  justify-contents: center;
+  //border: solid 1px #e0e0e0;
+`;
+
+const StyledBox = styled.div`
+  text-align: center;
+  vertical-align: center;
+  font-size: 1em;
+  border: solid 1px #e0e0e0;
+  width: 100%;
+  height: 70%;
+`;
+/*
+//const
+
+function CalendarGrid(props) {
+  var dayDiff = 1 + props.dateDiff / (24 * 60 * 60 * 1000);
+  var days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let startSplit = props.startDate.toString();
+  startSplit = startSplit.split(" ");
+  let finalSplit = props.finalDate.toString();
+  finalSplit = finalSplit.split(" ");
+  const startIndex = days.indexOf(startSplit[0]);
+  //const finalIndex = days.indexOf(finalSplit[0]);
+
+  console.log(`day diff: ${dayDiff}`);
+
+  var start = {
+    year: startSplit[3],
+    month: startSplit[1],
+    day: startSplit[2],
+    dayName: startSplit[0],
+    startDayIndex: startIndex,
+  };
+  console.log(start.startDayIndex);
+  var final = {
+    year: finalSplit[3],
+    month: finalSplit[1],
+    day: finalSplit[2],
+    dayName: finalSplit[0],
+  };
+
+  //calculations for loops
+  if (7 - start.startDayIndex < dayDiff) {
+    var numberOfWeeks = 1 + (dayDiff - 7) / 7;
+    //var numberOfWeeks = 1 + (dayDiff - 7 + start.startDayIndex) / 7;
+    numberOfWeeks = Math.ceil(numberOfWeeks);
+  } else {
+    var numberOfWeeks = 1;
+  }
+
+  var weekNumber = [];
+  for (let i = 1; i <= numberOfWeeks; i++) {
+    weekNumber.push(i);
+  }
+  console.log(`number of weeks: ${numberOfWeeks}`);
+
+  //making time array
+  const startHour = Number(props.startHour);
+  const startMin = Number(props.startMin);
+  const startTime = startHour * 60 + startMin;
+  const finalHour = Number(props.finalHour);
+  const finalMin = Number(props.finalMin);
+  const finalTime = finalHour * 60 + finalMin;
+  const timeInterval = Number(props.duration);
+  var time = startTime;
+  const timeArray = [];
+
+  for (time; time <= finalTime; time += timeInterval) {
+    let hour = Math.floor(time / 60);
+    let min = time % 60;
+    if (min == 0) {
+      min = "00";
+    }
+    for (let i = 0; i < dayDiff; i++) {
+      timeArray.push(hour.toString() + ":" + min.toString());
+    }
+  }
+
+  //making days array
+  const daysArray = [];
+  let startDateData = props.startDate;
+  let finalDateData = props.finalDate;
+
+  for (let i = start.startDayIndex; i < dayDiff + start.startDayIndex; i++) {
+    daysArray.push(days[i % 7]);
+  }
+
+  //makind dates array
+  Date.prototype.addDays = function (days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+  };
+
+  function getDates(startDate, stopDate) {
+    var dateArray = new Array();
+    var currentDate = startDate;
+    while (currentDate <= stopDate) {
+      let cashe = new Date(currentDate);
+      cashe = currentDate.toString();
+      cashe = cashe.split(" ");
+      dateArray.push(cashe[2]);
+      currentDate = currentDate.addDays(1);
+    }
+    return dateArray;
+  }
+
+  const datesArray = getDates(startDateData, finalDateData);
+
+  console.log(`wtf?${props.startDate}`);
+  console.log(`month:${start.month}`);
+  console.log(`days:${daysArray}`);
+  console.log(`dates:${datesArray}`);
+
+  //makind months array
+  function getMonths(startDate, stopDate) {
+    var monthArray = new Array();
+    var currentDate = startDate;
+    while (currentDate <= stopDate) {
+      let cashe = new Date(currentDate);
+      cashe = currentDate.toString();
+      cashe = cashe.split(" ");
+      monthArray.push(cashe[1]);
+      currentDate = currentDate.addDays(1);
+    }
+  }
+
+  const monthsArray = getDates(startDateData, finalDateData);
+
+  //conbined Object
+  let combinedObject = [];
+
+  for (let i = 0; i < datesArray.length; i++) {
+    let object = {
+      month: monthsArray[i],
+      date: datesArray[i],
+      day: daysArray[i],
+      time: timeArray,
+    };
+    combinedObject.push(object);
+  }
+  console.log(`times:${combinedObject[0].time}`);
+*/
+
+function CalendarGrid(props) {
+  const numberOfWeeks = props.weeks;
+  // const datesArray = props.datesArray;
+  // const daysArray = props.daysArray;
+  // const monthsArray = props.monthsArray;
+  // const yearsArray = props.yearsArray;
+  // const timeArray = props.timeArray;
+  // var clickedArray = props.clickArray;
+  var displayArray = props.data;
+
+  // for (let i = 0; i < datesArray.length; i++) {
+  //   let temp = {};
+  //   temp.date = datesArray[i];
+  //   temp.day = daysArray[i];
+  //   temp.month = monthsArray[i];
+  //   temp.year = yearsArray[i];
+  //   temp.time = {};
+  //   temp.time = timeArray;
+  //   temp.clicked = clickedArray;
+  //   displayArray[i] = temp;
+  // }
+  // console.log("displayArray:");
+  // console.log(displayArray);
+
+  //const clickedArray = props.data[0].clicked;
+
+  //console.log(combinedObject);
+
+  // for (let items in combinedObject) {
+  //   let temp = items.slice("-");
+  //   yearsArray.push(temp[0]);
+  //   monthsArray.push(temp[1]);
+  //   datesArray.push(temp[2]);
+  //   daysArray.push(temp[3]);
+  // }
+
+  // console.log(yearsArray);
+  // console.log(monthsArray);
+  // console.log(datesArray);
+  // console.log(daysArray);
+  //States
+  //const [stateDate, setStateDate] = useState(0);
+
+  const [stateWeeks, setStateWeeks] = useState(0);
+  // const [stateDates, setStateDates] = useState(datesArray.slice(0, 7));
+  // const [stateDays, setStateDays] = useState(daysArray.slice(0, 7));
+  // const [stateTime, setStateTime] = useState(timeArray);
+  const [stateDates, setStateDates] = useState(displayArray.slice(0, 7));
+  // const [stateClicked, setStateClicked] = useState(clickedArray);
+  const [isHover, setHover] = useState(false);
+  console.log("stateDates:");
+  console.log(stateDates);
+  console.log("stateWeeks:");
+  console.log(stateWeeks);
+  const increaseWeek = () => {
+    setStateWeeks((stateWeeks + 1) % numberOfWeeks);
+    setStateDates(
+      displayArray.slice(
+        7 * ((stateWeeks + 1) % numberOfWeeks),
+        7 * ((stateWeeks + 1) % numberOfWeeks) + 7
+      )
+    );
+    // setStateDays(
+    //   daysArray.slice(
+    //     7 * ((stateWeeks + 1) % numberOfWeeks),
+    //     7 * ((stateWeeks + 1) % numberOfWeeks) + 7
+    //   )
+    // );
+  };
+  const decreaseWeek = () => {
+    setStateWeeks((stateWeeks + numberOfWeeks - 1) % numberOfWeeks);
+    setStateDates(
+      displayArray.slice(
+        7 * ((stateWeeks + numberOfWeeks - 1) % numberOfWeeks),
+        7 * ((stateWeeks + numberOfWeeks - 1) % numberOfWeeks) + 7
+      )
+    );
+    // setStateDays(
+    //   daysArray.slice(
+    //     7 * ((stateWeeks + numberOfWeeks - 1) % numberOfWeeks),
+    //     7 * ((stateWeeks + numberOfWeeks - 1) % numberOfWeeks) + 7
+    //   )
+    // );
+  };
+
+  const makeHover = () => {
+    setHover(true);
+  };
+
+  const makeNotHover = () => {
+    setHover(false);
+  };
+
+  function setClick(array, i) {
+    array[i] = setState(!array[i]);
+  }
+
+  return (
+    <>
+      <HeaderWrapper>
+        <StyledWeek>
+          <span>
+            <GrIcons.GrFormPrevious size="1.5em" onClick={decreaseWeek} />
+          </span>
+          <span>Week {(stateWeeks % numberOfWeeks) + 1}</span>
+          <span>
+            <GrIcons.GrFormNext size="1.5em" onClick={increaseWeek} />
+          </span>
+          <StyledYear>
+            {"  "} {stateDates[0].year} {stateDates[0].month}
+          </StyledYear>
+        </StyledWeek>
+      </HeaderWrapper>
+      <GridContainer columns={stateDates.length}>
+        {stateDates.map((item, index) => {
+          return (
+            <FlexContainer>
+              <StyledDays>{item.day}</StyledDays>
+              <StyledDates>{item.date}</StyledDates>
+              {item.time.map((subitem, i) => {
+                return (
+                  <StyledBox onMouseOver={makeHover} onMouseOut={makeNotHover}>
+                    <CalendarButton
+                      time={subitem}
+                      clicked={item.clicked[i]}
+                      firstName="Han"
+                    />
+                  </StyledBox>
+                );
+              })}
+            </FlexContainer>
+          );
+        })}
+      </GridContainer>
+    </>
+  );
+
+  // console.log(`state week: ${stateWeeks}`);
+  // console.log(`statedates: ${stateDates}`);
+  // console.log(`stateDays: ${stateDays}`);
+  // return <HeaderWrapper>Hello</HeaderWrapper>;
+
+  // return (
+  //   <HeaderWrapper>
+  //     <HeaderWrapper>
+  //       <StyledWeek>
+  //         <span>
+  //           <GrIcons.GrFormPrevious size="1.5em" onClick={decreaseWeek} />
+  //         </span>
+  //         <span>Week {(stateWeeks % numberOfWeeks) + 1}</span>
+  //         <span>
+  //           <GrIcons.GrFormNext size="1.5em" onClick={increaseWeek} />
+  //         </span>
+  //         <StyledYear>
+  //           {"  "} {yearsArray[0]}
+  //         </StyledYear>
+  //       </StyledWeek>
+  //     </HeaderWrapper>
+  //     <GridContainer columns={stateDates.length}>
+  //       {stateDates.map((item, index) => {
+  //         return (
+  //           <StyledDates columns={stateDates.length}>
+  //             <b>{item}</b>
+  //           </StyledDates>
+  //         );
+  //       })}
+  //       {stateDays.map((item, index) => {
+  //         return <StyledDays>{item}</StyledDays>;
+  //       })}
+
+  //       {timeArray.map((item, index) => {
+  //         return (
+  //           <StyledBox>
+  //             {" "}
+  //             <CalendarButton time={item} firstName="han" />
+  //           </StyledBox>
+  //         );
+  //       })}
+  //     </GridContainer>
+  //   </HeaderWrapper>
+  // );
+}
+
+CalendarGrid.defaultProps = {
+  startDate: new Date(),
+  finalDate: new Date(),
+  dateDiff: 0,
+  startHour: 10,
+  startMin: 0,
+  finalHour: 16,
+  finalMin: 0,
+};
+
+export default CalendarGrid;
