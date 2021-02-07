e30fd2ea7164665f7511cb0ba3f03d78b86d823c
hanyu0104 <ubchanyu@gmail.com>
2021-02-07 20:17:15 +0900

    before modifying

--- src/components/ShareLink.js
+++ src/components/ShareLink.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import * as AiIcons from "react-icons/ai";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
@@ -27,10 +27,12 @@ const Title2 = styled.div`
 `;
 
 const FlexWrapper = styled.div`
+  margin-top: 1%;
   padding: 0.3em 0;
   display: inline-flex;
   justify-content: space-between;
-  align-items: start;
+  align-items: center;
+  border: solid 1px black;
 `;
 
 const Wrapper = styled.div`
@@ -53,23 +55,23 @@ const Recipients = styled.span`
   display: flex;
   justify-content: space-around;
   margin: 0.3%
-  background-color: ${(props) => props.color}
+  background-color: ${(props) => (props.color ? "#4f4f4f" : "#5845cb")}
 `;
 
 const Email = styled.span`
   font-size: 1.2em;
   font-weight: 600;
   margin: 0% 5%;
-  color: ${(props) => props.color};
-  text-decoration: ${(props) => props.deco};
+  color: ${(props) => (props.color ? "#5845cb" : "#4f4f4f")};
+  text-decoration: ${(props) => (props.deco ? "underline" : "")};
 `;
 
 const DirectLink = styled.span`
   font-size: 1.2em;
   font-weight: 500;
   margin: 0% 5%;
-  color: ${(props) => props.color};
-  text-decoration: ${(props) => props.deco};
+  color: ${(props) => (props.color ? "#4f4f4f" : "#5845cb")};
+  text-decoration: ${(props) => (props.deco ? "" : "underline")};
 `;
 
 const Copy = styled.span`
@@ -82,16 +84,24 @@ const Copy = styled.span`
   align-item: center;
   border-radius: 0.3em;
   box-shadow: 0.1em 0.1em 0em #e0e0e0;
-  visibility: ${(props) => props.visibility};
+  visibility: ${(props) => (props.visibility ? "hidden" : "visible")};
+  transition: hover 250ms;
+
+  &:hover {
+    opacity: 0.7;
+  }
 
   &:active {
     transform: translateY(3%);
     background-color: #4835bb;
+    opacity: 1;
   }
 `;
 
 const Box = styled.span`
+  overflow: auto;
   padding: 0.5% 1%;
+  height: fit-content;
   width: 86%;
   display: flex;
   justify-content: flex-start;
@@ -102,12 +112,34 @@ const Box = styled.span`
   font-weight: 500;
 `;
 
-const LinkBox = styled.span`
-  padding: 0.35em;
+const EmailGrid = styled.div`
+  display: grid;
+  row-auto-template: auto;
+  column-auto-template: auto;
 `;
 
-const EmailBox = styled.span`
-  padding: 0.5% 2%;
+const EmailBox = styled.textarea`
+  padding: 1em 1em;
+  margin-top: 1%;
+  width: 100%;
+  height: 50%;
+`;
+
+const LinkBox = styled.span`
+  padding: 0.5% 1%;
+  height: fit-content;
+  width: 86%;
+  display: flex;
+  justify-content: flex-start;
+  color: black;
+  border: solid 1px #e0e0e0;
+  border-radius: 0.3em;
+  font-size: 1.1em;
+  font-weight: 500; ;
+`;
+
+const AddressBox = styled.span`
+  padding: 0.2% 1%;
   display: inline-block;
   font: open-sans;
   background-color: #aeaeae;
@@ -117,7 +149,7 @@ const EmailBox = styled.span`
 
 const SubWrapper3 = styled.div`
   display: flex;
-  justify-content: flex-end;
+  //justify-content: flex-end;
   align-item: center;
 `;
 
@@ -134,7 +166,7 @@ const Reci = styled.span`
 `;
 
 const AddEmailBox = styled.div`
-  visibility: ${(props) => props.visibility};
+  visibility: ${(props) => (props.visibility ? "visible" : "hidden")};
   position: absolute;
   display: flex;
   flex-direction: column;
@@ -152,11 +184,21 @@ const AddEmailBox = styled.div`
 `;
 
 const IconBox = styled.div`
-  margin-right: 5%;
-  padding: 1em 0.2em;
-  width: 100%;
+  position: relative;
+  left: 46%;
+  top: 2%;
+  margin-bottom: 1em;
+  width: 2em;
+  height: 2em;
   display: flex;
-  justify-content: flex-end;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1em;
+  transition: background-color 250ms;
+
+  &:hover {
+    background-color: #f3f3f3;
+  }
 `;
 
 const Inst = styled.div`
@@ -168,27 +210,48 @@ const Inst = styled.div`
 `;
 
 const Input = styled.textarea`
-  height: 44%;
+  height: 50%;
   width: 81%;
   font-size: 1.2em;
   padding: 3% 3%;
   line-height: normal;
   font: open-sans;
   outline-style: none;
+  transition border 250ms;
+  &:focus {
+    outline: none;
+    border: 1px solid blue;
+  }
 `;
 
 const CopiedConfirm = styled.div`
-  visibility: ${(props) => props.visibility};
+  visibility: ${(props) => (props.visibility ? "visible" : "hidden")};
   font-size: 0.8em;
   font-weight: 500;
+  animation: Fadeout 1000ms;
+`;
+
+const Fadeout = keyframes`
+0%{
+  opacity: 1;
+}
+
+70%{
+  opacity: 0;
+}
+
+100$ {
+  opacity: 0;
+}
 `;
 
 function ShareLink(props) {
   const [recipients, setRecepients] = useState(props.recipients);
   const [isEmail, setIsEmail] = useState(true);
   const [directLink, setLink] = useState(props.directLink);
-  const [isCopied, setIsCopied] = useState(false);
-  const [recipientEmail, setRecipientEmail] = useState(null);
+  const [linkCopied, setLinkCopied] = useState(false);
+  const [emailCopied, setEmailCopied] = useState(false);
+  const [recipientsEmail, setRecipientEmail] = useState(props.recipientsEmail);
   const [addEmail, setAddEmail] = useState(false);
 
   const makeRecipients = () => {};
@@ -208,23 +271,27 @@ function ShareLink(props) {
     setAddEmail(false);
   };
 
-  const Copied = () => {
-    setIsCopied(true);
+  const CopyLink = () => {
+    setLinkCopied(true);
+  };
+
+  const CopyEmail = () => {
+    setEmailCopied(true);
   };
 
   console.log(isEmail);
 
-  if (isEmail && recipientEmail) {
+  if (isEmail) {
     return (
       <>
         {/* <GrayBG visibility={addEmail ? "visible" : "hidden"}> */}
-        <AddEmailBox visibility={addEmail ? "visible" : "hidden"}>
+        {/* <AddEmailBox visibility={addEmail}>
           <IconBox>
             <AiIcons.AiOutlineClose size="1.6em" onClick={closeAddEmail} />
           </IconBox>
           <Inst>Enter the email addresses separated with commas</Inst>
           <Input></Input>
-        </AddEmailBox>
+        </AddEmailBox> */}
         {/* </GrayBG> */}
         <Container>
           <Wrapper>
@@ -240,90 +307,93 @@ function ShareLink(props) {
             </FlexWrapper>
             <SubWrapper2>
               <Noti>Notify by</Noti>
-              <Email
-                onClick={makeEmail}
-                color={isEmail ? "#5845cb" : "#4f4f4f"}
-                deco={isEmail ? "underline" : ""}
-              >
+              <Email onClick={makeEmail} color={isEmail} deco={isEmail}>
                 Email
               </Email>
               <DirectLink
                 onClick={makeDirectLink}
-                color={isEmail ? "#4f4f4f" : "#5845cb"}
-                deco={isEmail ? "" : "underline"}
+                color={isEmail}
+                deco={isEmail}
               >
                 Direck Link
               </DirectLink>
             </SubWrapper2>
             <FlexWrapper>
-              <Copy visibility={isEmail ? "hidden" : "visible"}>Copy</Copy>
+              <span>To</span>
               <Box>
-                <EmailBox>EMAIL (to be alterned with state)</EmailBox>
+                {/* <AddressBox>EMAIL (to be alterned with state)</AddressBox> */}
               </Box>
             </FlexWrapper>
-            <SubWrapper3>
-              <Copy onClick={openAddEmail}>Add</Copy>
-            </SubWrapper3>
+            <FlexWrapper>
+              <span>From</span>
+              <Box></Box>
+            </FlexWrapper>
+            <FlexWrapper>
+              <span>Subject</span>
+              <Box></Box>
+            </FlexWrapper>
+
+            <EmailBox></EmailBox>
           </Wrapper>
         </Container>
       </>
     );
-  } else if (isEmail && recipientEmail == null) {
-    //Using email, no email state
-    return (
-      <>
-        {/* <GrayBG visibility={addEmail ? "visible" : "hidden"}> */}
-        <AddEmailBox visibility={addEmail ? "visible" : "hidden"}>
-          <IconBox>
-            <AiIcons.AiOutlineClose size="1.6em" onClick={closeAddEmail} />
-          </IconBox>
-          <Inst>Enter the email addresses separated with commas</Inst>
-          <Input></Input>
-        </AddEmailBox>
-        {/* </GrayBG> */}
-        <Container>
-          <Wrapper>
-            <ProjectTitle>{props.projectTitle}</ProjectTitle>
-            <Title2>Invite Applicants</Title2>
-            <FlexWrapper>
-              <Reci>Recipients</Reci>
-              <Box>
-                {recipients.map((recipient, index) => {
-                  return <Recipients>{recipient}</Recipients>;
-                })}
-              </Box>
-            </FlexWrapper>
-            <SubWrapper2>
-              <Noti>Notify by</Noti>
-              <Email
-                onClick={makeEmail}
-                color={isEmail ? "#5845cb" : "#4f4f4f"}
-                deco={isEmail ? "underline" : ""}
-              >
-                Email
-              </Email>
-              <DirectLink
-                onClick={makeDirectLink}
-                color={isEmail ? "#4f4f4f" : "#5845cb"}
-                deco={isEmail ? "" : "underline"}
-              >
-                Direct Link
-              </DirectLink>
-            </SubWrapper2>
-            <FlexWrapper>
-              <Copy visibility={isEmail ? "hidden" : "visible"}>Copy</Copy>
-              <Box>
-                <EmailBox onClick={openAddEmail}>Please Enter Email</EmailBox>
-              </Box>
-            </FlexWrapper>
-            <SubWrapper3>
-              <Copy onClick={openAddEmail}>Add</Copy>
-            </SubWrapper3>
-          </Wrapper>
-        </Container>
-      </>
-    );
-  } else {
+  }
+  // else if (isEmail && recipientEmail == null) {
+  //   //Using email, no email state
+  //   return (
+  //     <>
+  //       {/* <GrayBG visibility={addEmail ? "visible" : "hidden"}> */}
+  //       <AddEmailBox visibility={addEmail}>
+  //         <IconBox>
+  //           <AiIcons.AiOutlineClose size="1.6em" onClick={closeAddEmail} />
+  //         </IconBox>
+  //         <Inst>Enter the email addresses separated with commas</Inst>
+  //         <Input></Input>
+  //       </AddEmailBox>
+  //       {/* </GrayBG> */}
+  //       <Container>
+  //         <Wrapper>
+  //           <ProjectTitle>{props.projectTitle}</ProjectTitle>
+  //           <Title2>Invite Applicants</Title2>
+  //           <FlexWrapper>
+  //             <Reci>Recipients</Reci>
+  //             <Box>
+  //               {recipients.map((recipient, index) => {
+  //                 return <Recipients>{recipient}</Recipients>;
+  //               })}
+  //             </Box>
+  //           </FlexWrapper>
+  //           <SubWrapper2>
+  //             <Noti>Notify by</Noti>
+  //             <Email onClick={makeEmail} color={isEmail} deco={isEmail}>
+  //               Email
+  //             </Email>
+  //             <DirectLink
+  //               onClick={makeDirectLink}
+  //               color={isEmail}
+  //               deco={isEmail}
+  //             >
+  //               Direct Link
+  //             </DirectLink>
+  //           </SubWrapper2>
+  //           <FlexWrapper>
+  //             <Copy visibility={isEmail}>Copy</Copy>
+  //             <Box>
+  //               <AddressBox onClick={openAddEmail}>
+  //                 Please Enter Email
+  //               </AddressBox>
+  //             </Box>
+  //           </FlexWrapper>
+  //           <SubWrapper3>
+  //             <Copy onClick={openAddEmail}>Add</Copy>
+  //           </SubWrapper3>
+  //         </Wrapper>
+  //       </Container>
+  //     </>
+  //   );
+  // }
+  else {
     //using direct link
     return (
       <>
@@ -341,36 +411,41 @@ function ShareLink(props) {
             </FlexWrapper>
             <SubWrapper2>
               <Noti>Notify by</Noti>
-              <Email
-                onClick={makeEmail}
-                color={isEmail ? "#5845cb" : "#4f4f4f"}
-                deco={isEmail ? "underline" : ""}
-              >
+              <Email onClick={makeEmail} color={isEmail} deco={isEmail}>
                 Email
               </Email>
               <DirectLink
                 onClick={makeDirectLink}
-                color={isEmail ? "#4f4f4f" : "#5845cb"}
-                deco={isEmail ? "" : "underline"}
+                color={isEmail}
+                deco={isEmail}
               >
                 Direct Link
               </DirectLink>
             </SubWrapper2>
             <FlexWrapper>
-              <CopyToClipboard onClick={Copied} text={directLink}>
-                <Copy
-                  onClick={Copied}
-                  visibility={isEmail ? "hidden" : "visible"}
-                >
+              <CopyToClipboard onClick={CopyLink} text={directLink}>
+                <Copy onClick={CopyLink} visibility={isEmail}>
                   Copy
                 </Copy>
               </CopyToClipboard>
+
+              <LinkBox>{directLink}</LinkBox>
+            </FlexWrapper>
+            <CopiedConfirm visibility={linkCopied}>
+              Link copied to clipboard!
+            </CopiedConfirm>
+            <FlexWrapper>
+              <CopyToClipboard onClick={CopyEmail} text={recipientsEmail}>
+                <Copy onClick={CopyEmail}> Copy</Copy>
+              </CopyToClipboard>
               <Box>
-                <LinkBox>{directLink}</LinkBox>
+                {recipientsEmail.map((email, index) => {
+                  return <AddressBox>{email},&nbsp;</AddressBox>;
+                })}
               </Box>
             </FlexWrapper>
-            <CopiedConfirm visibility={isCopied ? "visible" : "hidden"}>
-              Link copied to clipboard!
+            <CopiedConfirm visibility={emailCopied}>
+              Email copied to clipboard!
             </CopiedConfirm>
           </Wrapper>
         </Container>
@@ -383,6 +458,15 @@ ShareLink.defaultProps = {
   projectTitle: "Title",
   recipients: ["IGEN330", "Han", "Niko", "abc@gmail.com"],
   directLink: "www.google.com",
+  recipientsEmail: [
+    "ubchanyu@gmail.com",
+    "ubcniko@gmail.com",
+    "ubcjenny@gmail.com",
+    "ubckaylee@gmail.com",
+    "ubcabdullah@gmail.com",
+    "ubcmika@gmail.com",
+    "asdfasfdsafsfdsfdsfsdkjfhdslkjfhsl@alkdsjh",
+  ],
 };
 
 export default ShareLink;
