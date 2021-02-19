f5ae8e9f7d28c5d4d2d44621f0620cb15ba05280
hanyu0104 <ubchanyu@gmail.com>
2021-02-19 16:57:36 +0900

    OverviewCalendar

---
+++ src/components/CalendarButton3.js
@@ -0,0 +1,196 @@
+import React, { useState, useEffect } from "react";
+import CalendarButton3 from "./CalendarButton3.js";
+import IntervieweeCalendar from "./IntervieweeCalendar";
+import Calendar from "react-calendar";
+import * as GrIcons from "react-icons/gr";
+import { ContactMailOutlined } from "@material-ui/icons";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+//Styled Components
+const Container = styled.div`
+  width: 100vw;
+  height: 100vw;
+  display: flex;
+  flex-direction: column;
+`;
+
+const GridContainer = styled.div`
+  margin-top: 5%;
+  margin-left: 5%;
+  //max-width: 70vw;
+  gap: 0;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const ClickableIcon = styled(FontAwesomeIcon)`
+  font-size: 1.3rem;
+  cursor: pointer;
+  padding: 10px;
+  border-radius: 100%;
+  transition: all 250ms;
+  :hover {
+    background: #f3f3f3;
+  }
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: ${(props) => (props.enlarge ? "16vw" : "7vw")};
+`;
+
+const HeaderWrapper = styled.div`
+  height: 4em;
+  display: flex;
+  align-items: center;
+`;
+
+const StyledDates = styled.div`
+  font-size: 2em;
+  font: open-sans;
+  font-weight: 500;
+  color: #4f4f4f;
+  text-align: center;
+  vertical-align: center;
+  align-items: center;
+  justify-contents: center;
+  margin: 0 0 -1px -1px;
+  border-left: solid 1px #e0e0e0;
+  border-right: solid 1px #e0e0e0;
+  border-bottom: solid 1px #e0e0e0;
+`;
+
+const StyledWeek = styled.h3`
+  font: open-sans;
+  backgound-color: blue;
+  font-size: 1.5rem;
+  display: flex;
+  text-align: center;
+  vertical-align: center;
+  margin-top 5%;
+  margin-left 5%;
+  display: flex;
+  align-items: center;
+`;
+
+const StyledYear = styled.h3`
+  font-weight: 0.8em;
+  text-align: center;
+  vertical-align: center;
+  font-size: 1.5rem;
+  margin: 0;
+  font: sans-serif;
+`;
+
+const StyledDays = styled.div`
+  color: #4f4f4f;
+  font-size: 1em;
+  font: sans-serif;
+  text-align: center;
+  vertical-align: center;
+  align-items: center;
+  justify-contents: center;
+`;
+
+const StyledBox = styled.div`
+  text-align: center;
+  vertical-align: center;
+  font-size: 1em;
+  width: 100%;
+  height: 70%;
+`;
+
+function OverviewCalendar(props) {
+  const numberOfWeeks = props.weeks;
+  var displayArray = props.data;
+
+  const [stateWeeks, setStateWeeks] = useState(0);
+  const [stateDates, setStateDates] = useState(displayArray.slice(0, 7));
+
+  useEffect(() => {
+    setStateDates(displayArray.slice(0, 7));
+    setStateWeeks(0);
+  }, [displayArray]);
+
+  const increaseWeek = () => {
+    setStateWeeks((stateWeeks + 1) % numberOfWeeks);
+    setStateDates(
+      displayArray.slice(
+        7 * ((stateWeeks + 1) % numberOfWeeks),
+        7 * ((stateWeeks + 1) % numberOfWeeks) + 7
+      )
+    );
+  };
+  const decreaseWeek = () => {
+    setStateWeeks((stateWeeks + numberOfWeeks - 1) % numberOfWeeks);
+    setStateDates(
+      displayArray.slice(
+        7 * ((stateWeeks + numberOfWeeks - 1) % numberOfWeeks),
+        7 * ((stateWeeks + numberOfWeeks - 1) % numberOfWeeks) + 7
+      )
+    );
+  };
+
+  const makeEnlarge = (index) => {
+    displayArray[index + 7 * stateWeeks].enlarge = !displayArray[
+      index + 7 * stateWeeks
+    ].enlarge;
+    setStateDates(displayArray.slice(stateWeeks * 7, stateWeeks * 7 + 7));
+  };
+
+  return (
+    <>
+      <Container>
+        <HeaderWrapper>
+          <StyledWeek>
+            <ClickableIcon onClick={decreaseWeek} icon={faArrowLeft} />
+            <ClickableIcon onClick={increaseWeek} icon={faArrowRight} />
+            <span style={{ margin: "0 15px" }}>
+              Week {(stateWeeks % numberOfWeeks) + 1}
+            </span>
+            <StyledYear>
+              {"  "} {stateDates[0].year} {stateDates[0].month}
+            </StyledYear>
+          </StyledWeek>
+        </HeaderWrapper>
+        <GridContainer columns={stateDates.length}>
+          {stateDates.map((item, index) => {
+            return (
+              <FlexContainer enlarge={item.enlarge}>
+                <StyledDays>{item.day}</StyledDays>
+                <StyledDates>{item.date}</StyledDates>
+                {item.timeData.map((subitem, i) => {
+                  return (
+                    <div>
+                      <StyledBox
+                        onClick={() => {
+                          makeEnlarge(index);
+                        }}
+                      >
+                        <CalendarButton3
+                          time={subitem.time}
+                          interviewer={subitem.interviewer}
+                          interviewee={subitem.interviewee}
+                          enlarge={item.enlarge}
+                        />
+                      </StyledBox>
+                    </div>
+                  );
+                })}
+              </FlexContainer>
+            );
+          })}
+        </GridContainer>
+      </Container>
+    </>
+  );
+}
+
+OverviewCalendar.defaultProps = {
+  interviewer: "Han",
+};
+
+export default OverviewCalendar;
---
+++ src/components/OverviewCalendar.js
@@ -0,0 +1,191 @@
+import { HowToVoteRounded } from "@material-ui/icons";
+import React, { useState, useEffect } from "react";
+import styled, { keyframes } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const FlexWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  align-items: center;
+`;
+
+const ColumnWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const Container = styled.div`
+  height: 100px;
+  width: ${(props) => (props.enlarge ? "16vw" : "")};
+  display: block;
+  //flex-direction: column;
+  //cursor: pointer;
+  //justify-content: ${(props) => (props.enlarge ? "" : "space-between")};
+  //justify-content: space-between;
+  //color: #0e0e0e;
+  //padding: 5%;
+  transition: all 250ms;
+  position: relative;
+  border: solid 1px #e0e0e0;
+  //border-right: solid 1px #e0e0e0;
+  margin: 0 0 -1px -1px;
+  overflow: hide;
+
+  &:hover {
+    background-color: #f6f6f6;
+  }
+`;
+
+const Interviewer = styled.span`
+  margin-left: 5px;
+  display: inline-box;
+  background-color: ${(props) => props.bgcolor};
+  color: white;
+  width: fit-content;
+  border-radius: 5px;
+  padding: 3% 5% 3% 5%;
+  font-size: 0.8em;
+  font: open-sans;
+  font-weight: 650;
+`;
+
+const Interviewee = styled.span`
+  margin-left: 5px;
+  margin-top: 5px;
+  display: inline-box;
+  background-color: ${(props) => props.bgcolor};
+  color: white;
+  width: fit-content;
+  border-radius: 5px;
+  padding: 3% 5% 3% 5%;
+  font-size: 0.8em;
+  font: open-sans;
+  font-weight: 650;
+`;
+
+const Time = styled.div`
+  display: inline-box;
+  margin-top: 2px;
+  margin-right: 2px;
+  font-size: 1em;
+  font: open-sans;
+  font-weight: 400;
+  text-align: right;
+  align-items: start;
+  justify-contents: end;
+  //paddig-top: 2%;
+  //padding-bottom: 7%;
+  //padding-left: 10%;
+`;
+
+function CalendarButton3(props) {
+  const [interviewer, setInterviewer] = useState(props.interviewer);
+  const [interviewee, setInterviewee] = useState(props.interviewee);
+  const [enlarge, setEnlarge] = useState(props.enlarge);
+
+  useEffect(() => {
+    setEnlarge(props.enlarge);
+  }, [props.enlarge]);
+
+  const colors = ["#81c784", "#ffd54f", "#ff8a65", "#ba68c8", "#e57373"];
+
+  console.log(interviewer);
+  console.log(interviewee);
+
+  if (enlarge) {
+    return (
+      <Container enlarge={enlarge}>
+        <FlexWrapper style={{ justifyContent: "flex-end" }}>
+          <Time>{props.time}</Time>
+        </FlexWrapper>
+        <FlexWrapper>
+          {interviewer
+            ? [
+                interviewee ? (
+                  <ColumnWrapper>
+                    <FlexWrapper style={{ justifyContent: "flex-start" }}>
+                      <span>Interviewer: </span>
+                      <Interviewer bgcolor="#7986cb">
+                        {props.interviewer}
+                      </Interviewer>
+                    </FlexWrapper>
+                    <FlexWrapper style={{ justifyContent: "flex-start" }}>
+                      <span>Interviewee: </span>
+                      {interviewer}
+                      {/* {interviewee.map((item, i) => {
+                        return (
+                          <Interviewee bgcolor={colors[i]}>{item}</Interviewee>
+                        );
+                      })} */}
+                    </FlexWrapper>
+                  </ColumnWrapper>
+                ) : (
+                  <Interviewer bgcolor="#e0e0e0">{interviewer}</Interviewer>
+                ),
+              ]
+            : [
+                <>
+                  <Interviewer></Interviewer>
+                  <Interviewee></Interviewee>
+                </>,
+              ]}
+        </FlexWrapper>
+      </Container>
+    );
+  } else {
+    return (
+      <Container enlarge={enlarge}>
+        <FlexWrapper style={{ justifyContent: "flex-end" }}>
+          <Time>{props.time}</Time>
+        </FlexWrapper>
+
+        <FlexWrapper>
+          {interviewer
+            ? [
+                interviewee ? (
+                  <ColumnWrapper>
+                    <FlexWrapper style={{ justifyContent: "flex-start" }}>
+                      <Interviewer bgcolor="#7986cb">
+                        {interviewer.charAt(0)}
+                      </Interviewer>
+                    </FlexWrapper>
+                    <FlexWrapper style={{ justifyContent: "flex-start" }}>
+                      <Interviewee>{interviewee}</Interviewee>
+                      {/* {interviewee.map((item, i) => {
+                        console.log(item);
+                        return (
+                          <Interviewee bgcolor={colors[i]}>
+                            {item.charAt(0)}
+                          </Interviewee>
+                        );
+                      })} */}
+                    </FlexWrapper>
+                  </ColumnWrapper>
+                ) : (
+                  <Interviewer bgcolor="#e0e0e0">
+                    {interviewer.charAt(0)}
+                  </Interviewer>
+                ),
+              ]
+            : [
+                <>
+                  <Interviewer></Interviewer>
+                  <Interviewee></Interviewee>
+                </>,
+              ]}
+        </FlexWrapper>
+      </Container>
+    );
+  }
+}
+
+CalendarButton3.defaultProps = {
+  time: "no time",
+  //interviewer: "Han",
+  //interviewee: ["Niko", "Jenny", "Kaylee"],
+};
+
+export default CalendarButton3;
